acb7cd706359713fcc3b300b3c18c95e244b00d7
Maxat Kulmanov <coolmaksat@gmail.com>
2023-09-18 13:48:11 +0300

    upload data

--- upload_data.sh
+++ upload_data.sh
@@ -1,8 +1,8 @@
 for ont in mf bp cc; do
     echo $ont
-    scp data/$ont/terms.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgo-cafa5/data/$ont/
-    scp data/$ont/interpros.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgo-cafa5/data/$ont/
-    scp data/$ont/*_data.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgo-cafa5/data/$ont/
-    scp data/$ont/*_data_diam.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgo-cafa5/data/$ont/
+    scp data/$ont/terms.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgometa/data/$ont/
+    scp data/$ont/interpros.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgometa/data/$ont/
+    scp data/$ont/*_data.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgometa/data/$ont/
+    scp data/$ont/*_data_diam.pkl glogin.ibex.kaust.edu.sa:/ibex/user/kulmanm/deepgometa/data/$ont/
 done
 
